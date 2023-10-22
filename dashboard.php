36bf2e390f43e94aa5836d73e6bea27c4635867f
parthwps <parth@netdevs.net>
2023-10-22 15:31:03 +0530

    dashboard

--- dashboard.php
+++ dashboard.php
@@ -2,36 +2,71 @@
 <?php include("header.php");?>
 
 <div class="container main dashboard">
-    <h3>Select Rooms in Your Home</h3>
     <nav>
     <div class="nav nav-tabs" id="nav-tab" role="tablist" style="white-space: nowrap;">
-        <button class="nav-link active" id="nav-1" data-bs-toggle="tab" data-bs-target="#nav1" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Foyer</button>
-        <button class="nav-link" id="nav-2" data-bs-toggle="tab" data-bs-target="#nav2" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Passage</button>
-        <button class="nav-link" id="nav-3" data-bs-toggle="tab" data-bs-target="#nav3" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Living Room</button>
-        <button class="nav-link" id="nav-4" data-bs-toggle="tab" data-bs-target="#nav4" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Drawing Room</button>
-        <button class="nav-link" id="nav-5" data-bs-toggle="tab" data-bs-target="#nav5" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Dining Room</button>
-        <button class="nav-link" id="nav-6" data-bs-toggle="tab" data-bs-target="#nav6" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Kitchen</button>
-        <button class="nav-link" id="nav-6" data-bs-toggle="tab" data-bs-target="#nav6" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Bedroom 1</button>
-        <button class="nav-link" id="nav-6" data-bs-toggle="tab" data-bs-target="#nav6" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Bedroom 2</button>
-        <button class="nav-link" id="nav-6" data-bs-toggle="tab" data-bs-target="#nav6" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Bedroom 3</button>
-        <button class="nav-link" id="nav-6" data-bs-toggle="tab" data-bs-target="#nav6" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Bathroom</button>
-        <button class="nav-link" id="nav-6" data-bs-toggle="tab" data-bs-target="#nav6" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Garden / Outside Entrance / Balcony</button>
+        <button style="background-color:#cce7d3;" class="nav-link active" id="nav-1" data-bs-toggle="tab" data-bs-target="#nav1" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Light Switch</button>
+        <button style="background-color:#feeef0;" class="nav-link" id="nav-2" data-bs-toggle="tab" data-bs-target="#nav2" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Plug</button>
+        <button style="background-color:#adebff;" class="nav-link" id="nav-3" data-bs-toggle="tab" data-bs-target="#nav3" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Scenario</button>
 
     </div>
     </nav>
     <div class="tab-content" id="nav-tabContent">
-        <div class="tab-pane fade show active" id="nav1" role="tabpanel" aria-labelledby="nav-1" tabindex="0">
-            <div class="tab1-lightswitch">
-                <h4>Light Switch</h4>
-            </div>
+        <div class="tab-pane fade show active" id="nav1" role="tabpanel" aria-labelledby="nav-1" tabindex="0" style="background-color:#cce7d3;">
+
+    <div class="form-check">
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+    <label class="form-check-label" for="flexCheckDefault">
+        Down Light
+    </label>
+    </div>
+    <div class="form-check">
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+    <label class="form-check-label" for="flexCheckDefault">
+        Wall Light
+    </label>
+    </div>
+    <div class="form-check">
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+    <label class="form-check-label" for="flexCheckDefault">
+        Panel Light
+    </label>
+    </div>
+    <div class="form-check">
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+    <label class="form-check-label" for="flexCheckDefault">
+        Rope Light
+    </label>
+    </div>
+    <div class="form-check">
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+    <label class="form-check-label" for="flexCheckDefault">
+        <input type="text" class="form-control mobile-input otpf">
+    </label>
+    </div>
+    <div class="form-check">
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+    <label class="form-check-label" for="flexCheckDefault">
+        <input type="text" class="form-control mobile-input otpf">
+    </label>
+    </div>
+    <div class="form-check">
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+    <label class="form-check-label" for="flexCheckDefault">
+        <input type="text" class="form-control mobile-input otpf">
+    </label>
+    </div>
+    <div class="form-check">
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+    <label class="form-check-label" for="flexCheckDefault">
+        <input type="text" class="form-control mobile-input otpf">
+    </label>
+    </div>
+
         </div>
-        <div class="tab-pane fade" id="nav2" role="tabpanel" aria-labelledby="nav-2" tabindex="1">
+        <div style="background-color:#feeef0;" class="tab-pane fade" id="nav2" role="tabpanel" aria-labelledby="nav-2" tabindex="1">
             test2
         </div>
-        <div class="tab-pane fade" id="nav3" role="tabpanel" aria-labelledby="nav-3" tabindex="2">
-            ...
-        </div>
-        <div class="tab-pane fade" id="nav4" role="tabpanel" aria-labelledby="nav-4" tabindex="3">
+        <div style="background-color:#adebff;" class="tab-pane fade" id="nav3" role="tabpanel" aria-labelledby="nav-3" tabindex="2">
             ...
         </div>
     </div>
@@ -44,6 +79,6 @@ let totalWidth = 0;
 for (let i = 0; i < items.length; i++) {
   totalWidth += items[i].offsetWidth;
 }
-container.style.width = totalWidth + 200 + "px";
+container.style.width = totalWidth+ 50 + "px";
 </script>
 <?php include("footer.php");?>
--- header.php
+++ header.php
@@ -1,7 +1,40 @@
 <nav class="navbar bg-body-tertiary">
-  <div class="container">
-    <a class="navbar-brand" href="#">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">
       <img src="assets/img/logo.png" alt="Bootstrap" class="logo">
     </a>
-  </div>
 </nav>
+
+<div class="container select-room d-flex justify-content-between mt-3">
+<h1>Foyer</h1>
+<button class="btn btn-link" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+  Select/Change Room
+</button>
+<div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Select Rooms</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item"><a class="nav-link" href="#">Foyer</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Passage</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Living Room</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Drawing Room</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Dining Room</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Kitchen</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Bedroom 1</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Bedroom 2</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Bedroom 3</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Bathroom</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Garden / Outside Entrance / Balcony</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+
+</div>
