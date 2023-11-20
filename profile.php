88d98f97139fc5b9405ff9050899c160b2ededbc
parthwps <parth@netdevs.net>
2023-11-20 17:37:28 +0530

    profi

--- profile.php
+++ profile.php
@@ -33,7 +33,7 @@
         var lastName = $("#lastName").val();
 
         // Check if first name and last name are not blank
-        if (firstName.replace(/\s+/g, ' ') === '' || lastName.replace(/\s+/g, ' ') === '') {
+        if (firstName.trim() === '' || lastName.trim() === '') {
             alert("First name and last name cannot be blank.");
             return;
         }
