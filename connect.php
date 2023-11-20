da68f3839f531475a10ae986651087e70073bbf0
parthwps <parth@netdevs.net>
2023-11-20 13:16:48 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,18 @@
 <?php
-$host = 'localhost';
-$dbname = 'alliee';
-$username = 'root';
-$password = '';
+
+if ($_SERVER['HTTP_HOST'] == 'localhost') {
+    $host = 'localhost';
+    $dbname = 'alliee';
+    $username = 'root';
+    $password = '';
+} else {
+    $host = 'charmiel_alliee';
+    $dbname = 'charmiel_alliee';
+    $username = 'root';
+    $password = 'Balaji@1234';
+}
+
+
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
