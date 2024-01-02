7639b9b199e0ecd339fd0bc14617ff34507f502c
parthwps <parth@netdevs.net>
2024-01-02 17:21:33 +0530

    Update fetch_suggestions.php

--- fetch_suggestions.php
+++ fetch_suggestions.php
@@ -1,9 +1,7 @@
 <?php
 require("connect.php");
 
-// Assuming $_POST["data"] is an array containing key-value pairs
 $data = $_GET["data"];
-// Define a list of keys
 $allowedKeys = array(
     'switch',
     'hl_switch',
@@ -15,11 +13,8 @@ $allowedKeys = array(
     'curtain'
 );
 
-// Initialize an array to store conditions
 $conditions = array();
 $conditions1 = array();
-
-//"SELECT * FROM panel_sugg WHERE (switch = :switch OR scenario = :scenario OR plug = :plug) AND (hl_switch = 0 AND fan = 0 AND dimmer = 0 AND tunable = 0 AND curtain = 0)[{"id":"1","name":"1 Switch","module":"2 Module","type":"s","switch":"1","hl_switch":"0","scenario":"0","plug":"0","fan":"0","dimmer":"0","tunable":"0","curtain":"0","time":"2023-12-15 13:51:49"},{"id":"2","name":"4 Switch","module":"2 Module","type":"s","switch":"4","hl_switch":"0","scenario":"0","plug":"0","fan":"0","dimmer":"0","tunable":"0","curtain":"0","time":"2023-12-15 13:51:49"},{"id":"4","name":"4 Scenario","module":"2 Module","type":"ss","switch":"0","hl_switch":"0","scenario":"4","plug":"0","fan":"0","dimmer":"0","tunable":"0","curtain":"0","time":"2023-12-15 13:51:49"},{"id":"5","name":"2 Switch + 2 Scenario","module":"2 Module","type":"ss","switch":"2","hl_switch":"0","scenario":"2","plug":"0","fan":"0","dimmer":"0","tunable":"0","curtain":"0","time":"2023-12-15 13:51:49"},{"id":"6","name":"6 Switch + 2 Scenario","module":"4 Module","type":"ss","switch":"6","hl_switch":"0","scenario":"2","plug":"0","fan":"0","dimmer":"0","tunable":"0","curtain":"0","time":"2023-12-15 13:51:49"},{"id":"7","name":"8 Switch + 2 Scenario","module":"6 & 8 Module","type":"ss","switch":"8","hl_switch":"0","scenario":"2","plug":"0","fan":"0","dimmer":"0","tunable":"0","curtain":"0","time":"2023-12-15 13:51:49"}]"
 
 foreach ($allowedKeys as $allowedKey) {
     if (array_key_exists($allowedKey, $data)) {
@@ -38,8 +33,6 @@ try {
     $query = "SELECT * FROM panel_sugg WHERE ($conditionsString) AND ($conditionsString1)";
     $stmt = $pdo->prepare($query);
     echo $query;
-// echo "SELECT * FROM panel_sugg WHERE ($conditionsString) AND ($conditionsString1)";
-    // Bind the values to the placeholders
     foreach ($allowedKeys as $allowedKey) {
         if (array_key_exists($allowedKey, $data)) {
             $stmt->bindValue(":$allowedKey", $data[$allowedKey]);
