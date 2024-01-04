921ae0d7e9d96f2fc7facd1a9405e15aa11263e3
parthwps <parth@netdevs.net>
2024-01-04 13:59:20 +0530

    Update fetch_suggestions.php

--- fetch_suggestions.php
+++ fetch_suggestions.php
@@ -1,45 +1,46 @@
 <?php
-require("connect.php");
-$data = $_GET["data"];
-$allowedKeys = array(
-    'switch',
-    'hl_switch',
-    'scenario',
-    'plug',
-    'fan',
-    'dimmer',
-    'tunable',
-    'curtain'
-);
+echo "good";
+// require("connect.php");
+// $data = $_GET["data"];
+// $allowedKeys = array(
+//     'switch',
+//     'hl_switch',
+//     'scenario',
+//     'plug',
+//     'fan',
+//     'dimmer',
+//     'tunable',
+//     'curtain'
+// );
 
-$conditions = array();
-$conditions1 = array();
+// $conditions = array();
+// $conditions1 = array();
 
-foreach ($allowedKeys as $allowedKey) {
-    if (array_key_exists($allowedKey, $data)) {
-        $conditions[] = "$allowedKey = :$allowedKey";
-    }
-}
-$conditionsString = implode(" OR ", $conditions);
-foreach ($allowedKeys as $allowedKey) {
-    if (array_key_exists($allowedKey, $data)) {
-    }else{
-        $conditions1[] = "$allowedKey = 0";
-    }
-}
-$conditionsString1 = implode(" AND ", $conditions1);
-try {
-    $query = "SELECT * FROM panel_sugg WHERE ($conditionsString) AND ($conditionsString1)";
-    $stmt = $pdo->prepare($query);
-    foreach ($allowedKeys as $allowedKey) {
-        if (array_key_exists($allowedKey, $data)) {
-            $stmt->bindValue(":$allowedKey", $data[$allowedKey]);
-        }
-    }
-    $stmt->execute();
-    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    echo json_encode($result);
-} catch (PDOException $e) {
-    echo "Query failed: " . $e->getMessage();
-}
+// foreach ($allowedKeys as $allowedKey) {
+//     if (array_key_exists($allowedKey, $data)) {
+//         $conditions[] = "$allowedKey = :$allowedKey";
+//     }
+// }
+// $conditionsString = implode(" OR ", $conditions);
+// foreach ($allowedKeys as $allowedKey) {
+//     if (array_key_exists($allowedKey, $data)) {
+//     }else{
+//         $conditions1[] = "$allowedKey = 0";
+//     }
+// }
+// $conditionsString1 = implode(" AND ", $conditions1);
+// try {
+//     $query = "SELECT * FROM panel_sugg WHERE ($conditionsString) AND ($conditionsString1)";
+//     $stmt = $pdo->prepare($query);
+//     foreach ($allowedKeys as $allowedKey) {
+//         if (array_key_exists($allowedKey, $data)) {
+//             $stmt->bindValue(":$allowedKey", $data[$allowedKey]);
+//         }
+//     }
+//     $stmt->execute();
+//     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+//     echo json_encode($result);
+// } catch (PDOException $e) {
+//     echo "Query failed: " . $e->getMessage();
+// }
 ?>
